bd65c99763539bd394706ac9313c0f07a023e1df
rosindubh <p.welsby2@ntlworld.com>
2021-08-14 14:25:53 +0100

    got rid of delay on button press

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 /*phil welsby 14 aug 2021 - playing with Audio*/
 const boom = document.getElementById("boom");
-const audio = document.getElementById("audio");
+//const audio = document.getElementById("audio");
 
 
 boom.addEventListener("click", () => {
     console.log("Boom here!");
     audio.play(boom);
+    audio.currentTime = 0;
 })
