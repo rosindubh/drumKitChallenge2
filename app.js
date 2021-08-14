87a9a32c0cf85889d588487b806617d6d7bf7386
rosindubh <p.welsby2@ntlworld.com>
2021-08-14 14:33:54 +0100

    write 9 variables for the instruments in js file

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 /*phil welsby 14 aug 2021 - playing with Audio*/
 const boom = document.getElementById("boom");
+const clap = document.getElementById("clap");
+const hiHat = document.getElementById("hihat");
+const kick = document.getElementById("kick");
+const openHat = document.getElementById("openhat");
+const ride = document.getElementById("ride");
+const snare = document.getElementById("snare");
+const tink = document.getElementById("tink");
+const tom = document.getElementById("tom");
+
 //const audio = document.getElementById("audio");
+
 
 
 boom.addEventListener("click", () => {
@@ -8,3 +18,4 @@ boom.addEventListener("click", () => {
     audio.play(boom);
     audio.currentTime = 0;
 })
+
