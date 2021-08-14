2e27eea5a0a7e583722f98809934ad2bd0bc41d2
rosindubh <p.welsby2@ntlworld.com>
2021-08-14 14:22:57 +0100

    got some sound - plus 404 error

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 /*phil welsby 14 aug 2021 - playing with Audio*/
+const boom = document.getElementById("boom");
+const audio = document.getElementById("audio");
+
+
+boom.addEventListener("click", () => {
+    console.log("Boom here!");
+    audio.play(boom);
+})
